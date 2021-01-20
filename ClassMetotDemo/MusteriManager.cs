eb4f11a3deeb6965df37aa8523f1c0169a0ee832
DOĞUKAN <DOUKAN@LAPTOP-ANIRAT8B>
2021-01-20 18:26:32 +0300

    Uygulamanın tamamlanmış hâli.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        // Müşteri eklendiğinde bilgi verecek olan metot.
+        public void Add(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad + " " + musteri.Soyad + " adlı müşteri eklendi.");
+        }
+
+        // Müşterileri listeleyecek olan metot.
+        public void List(List<Musteri> musteri)
+        {
+            Console.WriteLine("Müşteri Adı\t" + "Müşteri Soyadı\t" + "Müşteri Doğum Tarihi");
+            Console.WriteLine("-----------------------------------------------------------------");
+
+            foreach (var person in musteri)
+            {
+                Console.WriteLine(person.Ad + "\t\t" + person.Soyad + "\t\t" + person.DogumTarihi);
+            }
+        }
+
+    }
+}
--- ClassMetotDemo/Program.cs
+++ ClassMetotDemo/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ClassMetotDemo
 {
@@ -6,7 +7,39 @@ namespace ClassMetotDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var musteriler = new List<Musteri>()
+            {
+                new Musteri()
+                {
+                    Ad="Engin",
+                    Soyad="Demiroğ",
+                    DogumTarihi = "22.01.1986"
+                },
+                new Musteri()
+                {
+                    Ad="Salih",
+                    Soyad="Demiroğ",
+                    DogumTarihi = "11.12.1987"
+                },
+                new Musteri()
+                {
+                    Ad="Doğukan",
+                    Soyad="Bilgin",
+                    DogumTarihi="01.01.1987"
+                }
+            };
+
+            MusteriManager musteriManager = new MusteriManager();
+
+            foreach (var musteri in musteriler)
+            {
+                musteriManager.Add(musteri);
+            }
+
+            Console.WriteLine("\n\n---------------MÜŞTERİ LİSTESİ------------------\n\n");
+
+            musteriManager.List(musteriler);
+
         }
     }
 }
